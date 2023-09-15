16feb26a5aa55bad711af540a5bed15560935353
Aldaing <aldairramon94@gmail.com>
2023-09-14 23:20:28 -0600

    No fue mucho trabajo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     
     return (
          <h1 className="font-black text-5xl text-center md:w-1/2 mx-auto">
-             Seguimiento de pacientes {''} 
+             Seguimiento de pacientes {''} con Github {''} 
              <span className="text-indigo-600">Veterinaria</span>
         </h1>
        
