9526d3fae8e5d8e60a8f4dacbb67efb0cea8b360
Aldaing <aldairramon94@gmail.com>
2023-09-14 23:12:02 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect} from 'react'
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
+
+function App() {
+  
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState([]);
+
+
+  useEffect(()=>{
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []
+      setPacientes(pacientesLS)
+    }
+ 
+    obtenerLS();
+  }, []);
+
+  useEffect(()=>{
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+  },[pacientes])
+
+  const eliminarPaciente = id =>{
+    const pacientesActualizdos = pacientes.filter( paciente => paciente.id !== id);
+    setPacientes(pacientesActualizdos)
+  }
+
+    return (
+        <div className="container mx-auto mt-20">
+          <Header />
+
+          <div className="mt-12 md:flex">
+            <Formulario
+              pacientes={pacientes}
+              setPacientes={setPacientes}
+              paciente={paciente}
+              setPaciente={setPaciente}
+            />
+            <ListadoPacientes 
+              pacientes={pacientes}
+              setPaciente={setPaciente}
+              eliminarPaciente={eliminarPaciente}
+            /> 
+          </div>  
+              
+        </div>
+    )
+}
+
+export default App
